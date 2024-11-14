39910ce6a058703dd552eec50a7cb1f361966054
Ruthvik Suvarnakanti <ruthviksuvarnakanti@Ruthviks-Air.lan>
2024-11-13 22:14:41 -0500

    first commit

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using StudentDb.Models;
+
+
+namespace StudentDb
+{
+	public class AppDbContext: DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Student> student { get; set; }
+		
+	}
+}
+
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace StudentDb.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+    }
+
+}
